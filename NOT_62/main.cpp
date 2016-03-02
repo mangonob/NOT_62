ebadc8f384718cbb5fa7f41e42cfdb4e25ea4e3a
mangonob <534393806@qq.com>
2016-03-02 18:23:46 +0800

    Initial Commit

---
+++ NOT_62/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  NOT_62
+//
+//  Created by Trinity on 16/3/2.
+//  Copyright © 2016年 Trinity. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
